77b8e7d2cdfc47b1fa655e11770cb7c6a875dffc
krustnic <avmityakov@gmail.com>
2024-04-18 16:43:24 +0300

    first commit

---
+++ a.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/a":
+            console.log('a');
+            
+            res.writeHead(200);
+            res.end('a page');
+            
+            break
+        case "/b":
+            console.log('b');
+    
+            res.writeHead(301, {
+                'Location': '/a'
+            });
+            res.end();
+            break
+    }
+}).listen(8081);
---
+++ b.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    switch (req.url) {
+        case "/a":
+            console.log('a');
+            
+            res.writeHead(301, {
+                'Location': '/b'
+            });
+            res.end();
+            break
+        case "/b":
+            console.log('b');
+            
+            res.writeHead(200);
+            res.end('b page');
+    
+            break
+    }
+}).listen(8081);
